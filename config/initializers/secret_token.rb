779416a71c9d24f71a3d60a1bccdd1a625761c1e
XEdP3X <mario@xedp3x.de>
2013-09-28 15:11:39 +0200

    Rails 4.0.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GCT::Application.config.secret_key_base = '079e9aa1cecf1f58d6907d80cb31095d02cd6656d0be07f4a363a9122626b9894ecec9ac1a20ab2384c6e6fdc89224930891ea9c263839198b6f8bff9731cde7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GCT::Application.config.session_store :cookie_store, key: '_GCT_session'

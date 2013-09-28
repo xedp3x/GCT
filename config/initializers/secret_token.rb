ea40ec41a446df03581d5f44b2ef2aa509b11035
XEdP3X <mario@xedp3x.de>
2013-09-28 14:51:11 +0200

    cleanup

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Gct::Application.config.secret_token = '8ff7ac2ca3baa3955537ebec59f3b26d402af8a03d89b511d9c8b438f1ced141f5554cc9d35d10e5ae289ce49017110a3fd03de08168d627aabd6359b77c9009'
